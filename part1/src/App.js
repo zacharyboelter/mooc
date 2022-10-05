f22dbae0604f3d73e3aa86bc483bcb6af88f2931
Zachary Boelter <zboelter@gmail.com>
2022-10-05 13:58:31 -0700

    cicero texts

--- part1/src/App.js
+++ part1/src/App.js
@@ -19,20 +19,28 @@ const Time = () => {
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello, {props.name}</p>
+      <p>
+        Hello, {props.name}. You are {props.age} years old. That is quite old.
+        </p>
+        <p>
+        Nec vero habere virtutem satis est quasi artem aliquam nisi utare; etsi ars quidem cum ea non utare scientia tamen ipsa teneri potest, virtus in usu sui tota posita est; usus autem eius est maximus civitatis gubernatio, et earum ipsarum rerum quas isti in angulis personant, reapse non oratione perfectio. nihil enim dicitur a philosophis, quod quidem recte honesteque dicatur, quod ab iis partum confirmatumque sit, a quibus civitatibus iura discripta sunt. unde enim pietas, aut a quibus religio? unde ius aut gentium aut hoc ipsum civile quod dicitur? unde iustitia fides aequitas? unde pudor continentia fuga turpi dinis adpetentia laudis et honestatis? unde in laboribus et periculis fortitudo? 
+        </p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Zachary'
+  const age = 33
+
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
-      <Hello name='Barbara S.' />
-      <Hello name='Timotee C.'/>
-      <Hello name='Zachary B.'/>
+      <Hello name='Barbara' age={44+13} />
+      <Hello name={name} age={age}/>
+     <br />
       <Time />
-    </div>
+    </>
   )
 }
 
