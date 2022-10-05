c79ffa5d770d66a537e49803b44b6b1175995345
Zachary Boelter <zboelter@gmail.com>
2022-10-05 13:48:21 -0700

    props passed to hello component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,37 @@
 
 
 
-const App = () => {
+const Time = () => {
   const now = new Date()
-  const a = 10
-  const b = 20
+  // const a = 10
+  // const b = 20
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
+      <p>The time is {now.toString()}</p>
+      {/* <p>
         {a} plus {b} is {a + b}
-      </p>
+      </p> */}
+    </div>
+  )
+}
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello, {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Barbara S.' />
+      <Hello name='Timotee C.'/>
+      <Hello name='Zachary B.'/>
+      <Time />
     </div>
   )
 }
