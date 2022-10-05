5f630f310901385edfba5958e81ce5c9838aec38
Zachary Boelter <zboelter@gmail.com>
2022-10-05 14:53:40 -0700

    move part components to content component

--- part1/sumbissions/course-info/src/App.js
+++ part1/sumbissions/course-info/src/App.js
@@ -7,15 +7,27 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part1 = (props) =>{
   return (
     <>
       <p>
         {props.part1} {props.exercises1}
       </p>
+    </>
+  )
+}
+const Part2 = (props) =>{
+  return (
+    <>
       <p>
         {props.part2} {props.exercises2}
       </p>
+    </>
+  )
+}
+const Part3 = (props) =>{
+  return (
+    <>
       <p>
         {props.part3} {props.exercises3}
       </p>
@@ -23,7 +35,25 @@ const Content = (props) => {
   )
 }
 
+
+const Content = (props) => {
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  return (
+    <>
+      <Part1 part1={part1} exercises1={exercises1}/>
+      <Part2 part2={part2} exercises2={exercises2}/>
+      <Part3 part3={part3} exercises3={exercises3}/>
+    </>
+  )
+}
+
 const Total = (props) => {
+  
   return (
     <>
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
@@ -33,25 +63,17 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}/>
+      <Content />
       <Total exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}/>
+             exercises2={exercises2}
+             exercises3={exercises3}/>
     </div>
   )
 }
