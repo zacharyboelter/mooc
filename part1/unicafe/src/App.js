df58c2cb997c4ed55bfa7bcbe26cc2fa7575240d
Zachary Boelter <zboelter@gmail.com>
2022-10-12 14:11:29 -0700

    initial commit

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
