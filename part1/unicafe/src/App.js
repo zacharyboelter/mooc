c626967244418b547bbf3d45d1550af5ccc6e9c8
Zachary Boelter <zboelter@gmail.com>
2022-10-12 14:33:39 -0700

    task one complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,13 @@
 import { useState } from 'react'
+import './styles.css'
+
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,11 +15,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      
+      <h2>give feedback</h2>
+      <br />
+      <Button className=''good-btn text='Good' handleClick={handleGoodClick}/>
+      <Button text='Neutral' handleClick={handleNeutralClick}/>
+      <Button text='Bad' handleClick={handleBadClick}/>
+      <br />
+      <h2>statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
     </div>
   )
 }
