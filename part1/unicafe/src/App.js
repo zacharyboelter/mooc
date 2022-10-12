83bda6562f8e857e98e49f18439f33876dd4e69d
Zachary Boelter <zboelter@gmail.com>
2022-10-12 14:44:41 -0700

    round the positive percent of responses to 3 decimal points

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -25,6 +25,8 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const all = good + neutral + bad
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -37,6 +39,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {(good + neutral + bad) / all}</p>
+      <p>postitive: {(good / all * 100).toFixed(3)}</p>
     </div>
   )
 }
