dde328e903a540912e3c8e5809a4e40ec548ba7d
Zachary Boelter <zboelter@gmail.com>
2022-10-31 15:11:27 -0700

    create new module for Note

--- part2/render-collection/render/src/App.js
+++ part2/render-collection/render/src/App.js
@@ -1,19 +1,17 @@
+import Note from './Note'
+
+
 const App = (props) => {
   const { notes } = props
 
   return (
     <div>
       <h1>Notes</h1>
-      <ol>
-      {/* notes[1].content */}
-      {/* notes[2].content */}
-      {/* notes[3].content */}
-      {/* Instead, map over the array and display each as an LI */}
+      <ul>
         {notes.map(note =>
-           <li>
-              {note.content}
-            </li>)}
-      </ol>
+           <Note key={note.id} note={note}/>
+        )}
+      </ul>
     </div>
   )
 }
--- part2/render-collection/render/src/index.js
+++ part2/render-collection/render/src/index.js
@@ -18,7 +18,7 @@ const notes = [
   },
   {
     id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
+    content: 'GET and POST are the most important methods of HTTP protocol!',
     date: '2019-05-30T19:20:14.298Z',
     important: true
   }
