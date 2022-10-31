bcb3b3f69982691fabe886ae08d382b760ed3fbc
Zachary Boelter <zboelter@gmail.com>
2022-10-31 16:48:49 -0700

    use reduce to get the total number of exercises and display info in the course component.

--- part2/course-info/src/components/Course.js
+++ part2/course-info/src/components/Course.js
@@ -2,12 +2,17 @@ import Header from './sections/Header'
 import Content from './sections/Content'
 
 const Course = ({course}) => {
+    const total = course.parts.reduce((prev, curr) => {
+        return prev + curr.exercises
+      }, 0
+      )
     console.log(course)
     return (
         <div>
             <Header name={course.name} />
             {course.parts.map(part => 
                 <Content key={part.id} name={part.name} exercises={part.exercises} />)}
+            <h3>Total number of exercises: {total}</h3>
         </div>
     )
 }
--- part2/course-info/src/components/sections/Content.js
+++ part2/course-info/src/components/sections/Content.js
@@ -4,6 +4,7 @@ const Content = ({name, exercises}) => {
     return (
         <div>
             <p>{name}: {exercises}</p>
+
         </div>
     )
 }
